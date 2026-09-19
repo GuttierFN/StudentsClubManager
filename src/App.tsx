cc8ca083abf1666aadb7c4f5d3282e08106228a7
GuttierFN <alexgc191205@gmail.com>
2026-09-19 22:33:03 +0300

    fix: маленькие правки синтаксиса

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CssBaseline, ThemeProvider, createTheme, Container } from '@mui/materia
 import Navbar from './components/Navbar';
 import ClubsList from './pages/ClubsList';
 import ClubDetails from './pages/ClubDetails';
-import Schedule from './pages/Schedule';
+import Timetable from './pages/Timetable';
 import Attendance from './pages/Attendance';
 
 const theme = createTheme({
@@ -20,7 +20,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<ClubsList />} />
             <Route path="/clubs/:id" element={<ClubDetails />} />
-            <Route path="/schedule" element={<Timetable />} />
+            <Route path="/timetable" element={<Timetable />} />
             <Route path="/attendance" element={<Attendance />} />
           </Routes>
         </Container>
