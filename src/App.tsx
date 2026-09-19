ef7cbb79408de4ed73095b676a6b7e4c1003fe6c
GuttierFN <alexgc191205@gmail.com>
2026-09-19 21:30:47 +0300

    add: подключение темы MUI и настройка маршрутизации

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssBaseline, ThemeProvider, createTheme, Container } from '@mui/material';
+import Navbar from './components/Navbar';
+import ClubsList from './pages/ClubsList';
+import ClubDetails from './pages/ClubDetails';
+import Schedule from './pages/Schedule';
+import Attendance from './pages/Attendance';
+
+const theme = createTheme({
+  palette: { primary: { main: '#1976d2' } },
+});
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Container sx={{ mt: 4, mb: 4 }}>
+          <Routes>
+            <Route path="/" element={<ClubsList />} />
+            <Route path="/clubs/:id" element={<ClubDetails />} />
+            <Route path="/schedule" element={<Timetable />} />
+            <Route path="/attendance" element={<Attendance />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+
+
+
